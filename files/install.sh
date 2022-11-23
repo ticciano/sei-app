2653027af7fd92a368477ed4cf7e29cdb76d44bb
ticciano <ticianodiniz@outlook.com>
2022-11-23 16:46:14 -0300

    Limpeza do arquivo install.sh

--- files/install.sh
+++ files/install.sh
@@ -13,9 +13,6 @@ yum -y install libpng libjpeg icu libX11 libXext libXrender xorg-x11-fonts-Type1
 
 # Instalação do PHP e demais extenções necessárias para o projeto
 yum install -y epel-release yum-utils
-
-# CENTOS 0.1
-# yum install -y http://mirror.team-cymru.com/remi/enterprise/remi-release-7.rpm
 yum-config-manager --enable remi-php73
 yum -y update
 
@@ -27,7 +24,7 @@ yum -y install php php-common php-cli php-pear php-bcmath php-gd php-gmp php-ima
     freetds freetds-devel php-mssql php-sodium \
     git nc gearmand libgearman-dev libgearman-devel mysql
 
-# CENTOS 0.2
+
 # Configuração do pacote de línguas pt_BR
 localedef pt_BR -i pt_BR -f ISO-8859-1
 
@@ -73,7 +70,6 @@ echo /usr/lib/oracle/12.2/client64/lib > /etc/ld.so.conf.d/oracle-instantclient.
 ldconfig
 
 # Install Oracle extensions
-#yum install -y php73-php-pecl-oci8
 
 yum install -y php-dev php-pear build-essential systemtap-sdt-devel 
 pecl channel-update pecl.php.net 
@@ -85,20 +81,6 @@ rm -rf  /files/instaladores \
         /opt/oracle/oracle-instantclient12.2-basic-12.2.0.1.0-1.x86_64.rpm \
         /opt/oracle/oracle-instantclient12.2-devel-12.2.0.1.0-1.x86_64.rpm \
         /opt/oracle/oracle-instantclient12.2-sqlplus-12.2.0.1.0-1.x86_64.rpm
-
-
-# install sodium
-#cd /sei/instaladores
-#tar -xvzf libsodium-1.0.18-stable.tar.gz 
-#cd libsodium-stable/
-#./configure
-#make
-#make install
-#ldconfig
-#pecl install -f libsodium
-#echo "extension=sodium.so" > /etc/php.d/uploadprogress.ini
-#cd -
-
 
 mkdir -p /sei/controlador-instalacoes/ /sei/arquivos_externos_sei/ /sei/certs
 
